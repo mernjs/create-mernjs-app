8e7f8650d70d5cffee0d59badaf20aee15c7844f
MernJs Community <mernjscommunity@gmail.com>
2021-04-10 02:34:01 +0530

    update

--- src/controllers/WelcomeController.js
+++ src/controllers/WelcomeController.js
@@ -3,8 +3,12 @@ const InitCommand	= require('../models/InitCommand')
 class WelcomeController {
 
     async index(req, res){
+        view(res, 'pages/welcome', 'Welcome', 'Welcome', [])
+    }
+
+    async projects(req, res){
         const data = await InitCommand.find()
-        view(res, 'pages/welcome', 'Welcome', 'Get All Projects Successfully', data)
+        view(res, 'pages/projects', 'Projects', 'Get All Projects Successfully', data)
     }
 
     async details(req, res){
--- src/routes/web.js
+++ src/routes/web.js
@@ -4,6 +4,10 @@ Route.route('/')
 	.get(WelcomeController.index)
 	.all(send405);
 
+Route.route('/projects')
+	.get(WelcomeController.projects)
+	.all(send405);
+
 Route.route('/project-details')
 	.get(WelcomeController.details)
 	.all(send405);
